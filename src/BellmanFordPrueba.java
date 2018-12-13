59066702df339909e5ef35d5588d42163dd46334
Abraham¬_¬ <AbrahamRiRa>
2018-12-13 15:20:29 -0600

    Se agregaron las clases y la prueba para el algoritmo

---
+++ src/BellmanFordPrueba.java
@@ -0,0 +1,26 @@
+
+import java.io.IOException;
+
+public class BellmanFordPrueba {
+
+	public static void main(String[] args) {
+		BellmanFord bellman;
+		try {
+			bellman = new BellmanFord();
+			bellman.relajoArista();
+	        if (bellman.ciclo()) {
+	            for (int i = 0; i < bellman.numeroVertices; i++) {
+	                System.out.println("Coste desde " + bellman.nodoOrigen + " a " + (i + 1) + " =>" + bellman.etiquetas[i]);
+	            }
+	            for (int i = 0; i < bellman.numeroVertices; i++) {
+	                System.out.println("El predecesor de " + (i + 1) + " es " + (bellman.predecesor[i] + 1));
+	            }
+	        } else {
+	            System.out.println("Hay un ciclo negativo");
+	        }
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
